60c228c1373b690c021253ab2c857355d35a8c02
audeliathp <audeliasender@gmail.com>
2020-09-01 19:43:45 +0200

    test gem rails_admin

---
+++ Downloads/PUUURFECT-master/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  root 'items#index'
+  devise_for :users
+  resources 'items'
+  resources 'users', only: [:show, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :charges
+end
